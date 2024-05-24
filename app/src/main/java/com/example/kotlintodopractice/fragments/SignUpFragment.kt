99bc11c0308cc1f19b2aee30a97562e7f14b672f
Jeferson David Cañon Melo <97969138+JeferProgramer@users.noreply.github.com>
2024-05-23 22:36:00 -0500

    changes

--- app/src/main/java/com/example/kotlintodopractice/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/kotlintodopractice/fragments/SignUpFragment.kt
@@ -23,7 +23,6 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
     }
